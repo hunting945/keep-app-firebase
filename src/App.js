059a4c9b726aa4fc843541ac75e7b3bbb19db6f9
hunterlai <hunter.lai.corp@outlook.com>
2021-05-05 01:56:34 +0800

    02_render_notes_in_keep

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
